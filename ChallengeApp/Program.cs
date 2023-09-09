93a2f1a16b16b09e22904be92f403049905f55d7
AndJ1205 <144378919+AndJ1205@users.noreply.github.com>
2023-09-09 07:59:01 +0200

    Zadanie domowe dzień 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+char sex = 'F';//kobieta - F, mężczyzna - M
+int age = 28;
+if (sex == 'F' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && sex == 'F' && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == 'M' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Osoba nie spełniająca kryteriów");
+}
