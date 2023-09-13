ea96c1fa918dfc55c0e8e5ebb0d71508f547a8ff
AndJ1205 <144378919+AndJ1205@users.noreply.github.com>
2023-09-13 16:28:26 +0200

    Zadanie domowe dzień 8

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,46 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+
+    {
+        [Test]
+        public void WhenNameLooksTheSame()
+        {
+            //Arange
+            var employee1 = GetEmployee("Adam");
+            var employee2 = GetEmployee("Adam");
+
+            // Act
+
+            // Assert
+            Assert.AreNotEqual(employee1, employee2);
+        }
+        private Employee GetEmployee(string firstName)
+        {
+            return new Employee(firstName);
+        }
+        [Test]
+        public void WhenLoginIsTheSame()
+        {
+            //Arange
+            var employee1 = GetEmployee("Adam");
+            var employee2 = GetEmployee("Adam");
+
+            // Act
+
+            // Assert
+            Assert.AreEqual(employee1.FirstName, employee2.FirstName);
+        }
+        [Test]
+        public void WhenAgeReturn_compareEmployees()
+        {
+            //Arange
+            int age1 = 25;
+            int age2 = 32;
+
+            //Assert
+            Assert.AreNotEqual(age1, age2);
+        }
+        
+    }
+}
