dd23d55b8738604360d44f453dd94deac3250417
AndJ1205 <144378919+AndJ1205@users.noreply.github.com>
2023-09-13 10:03:37 +0200

    oceny pracowników

---
+++ ChallengeApp/ChallengeApp/EmploeeClass.cs
@@ -0,0 +1,34 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        //Pole przechowujące informacje o pracowniku
+        private string FirstName { get; set; }
+        string LastName { get; set; }
+        private int Age { get; set; }
+        int[] Ratings { get; set; }
+
+        //Konstruktor klasy Employee
+        public Employee(string firstName, string lastName, int age, int[] ratings)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Ratings = ratings;
+        }
+        //Metoda obliczania sumy ocen pracownika
+        public int CalculateTotalRatings()
+        {
+            return Ratings.Sum();
+        }
+        //Metoda do wyświetlenia informacji o pracowniku
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine("Informacje o pracowniku:");
+            Console.WriteLine($"Imię: {FirstName}");
+            Console.WriteLine($"Nazwisko: {LastName}");
+            Console.WriteLine($"Wiek: {Age}");
+            Console.WriteLine($"Oceny pracownika: {string.Join(", ", Ratings)}");
+        }
+    }
+}
---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using ChallengeApp;
+
+Employee employee1 = new Employee("Adam", "Kowal", 25, new int[] { 8, 9, 7, 8, 1 });
+Employee employee2 = new Employee("Anna", "Mucha", 32, new int[] { 6, 6, 9, 8, 7 });
+Employee employee3 = new Employee("Damian", "Dąb", 28, new int[] { 9, 9, 8, 7, 8 });
+
+//tablica pracowników
+Employee[] employees = { employee1, employee2, employee3 };
+
+//znajdowanie pracownika z najwyższą sumą ocen
+Employee employeeWithHigestRatings = employees.OrderByDescending(e => e.CalculateTotalRatings()).First();
+
+//wyświetlenie informacji o pracowniku z najwyższą sum,a ocen
+Console.WriteLine("Pracownik z najwyższą sumą ocen: ");
+employeeWithHigestRatings.DisplayEmployeeInfo();
+int totalRaitings = employeeWithHigestRatings.CalculateTotalRatings();
+Console.WriteLine($"Suma ocen: {totalRaitings}");
