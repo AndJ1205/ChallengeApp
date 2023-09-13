1b6453f12859d1133fcb30aa55fbeccd38cacff2
AndJ1205 <144378919+AndJ1205@users.noreply.github.com>
2023-09-13 10:49:03 +0200

    Test jednostkowy

--- ChallengeApp/ChallengeApp.Tests/UnitTest1.cs
+++ ChallengeApp/ChallengeApp.Tests/UnitTest1.cs
@@ -18,5 +18,33 @@ namespace ChallengeApp.Tests
             Assert.AreEqual(0, totalRatings); // Oczekujemy sumy ocen 0
 
         }
+        [Test]
+        public void WhenUserCollectScores_ShouldCorrectResultA()
+
+        {
+            //Arange
+            var employee = new Employee("Adam", "Kowal", 25, new int[] { -8, -9, 5, 7, -4 });
+
+            // Act
+            int totalRatings = employee.CalculateTotalRatings();
+
+            // Assert
+            Assert.AreEqual(-9, totalRatings); // Oczekujemy sumy ocen -9
+
+        }
+        [Test]
+        public void WhenUserCollectScores_ShouldCorrectResultB()
+
+        {
+            //Arange
+            var employee = new Employee("Adam", "Kowal", 25, new int[] { 8, 9, 5, 7, 4 });
+
+            // Act
+            int totalRatings = employee.CalculateTotalRatings();
+
+            // Assert
+            Assert.AreEqual(33, totalRatings); // Oczekujemy sumy ocen 33
+
+        }
     }
 }
